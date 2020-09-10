9f8a5c3960febf7a90ab749b70472792e4c42a42
DRISHTI SINGH <30665104+naina013@users.noreply.github.com>
2020-09-10 20:50:38 +0100

    CRUD Operations

--- Repository/IPostRepository.cs
+++ Repository/IPostRepository.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TMS.Models;
+using Task = TMS.Models.Task;
 
 namespace TMS.Repository
 {
     public interface IPostRepository
     {
-        
+        public Boolean Createtask(Task t);
+        public Task Readtask(int task_id);
+        public Boolean Updatetask(Task t);
+        public Boolean Deletetask(Task t);
+        public Boolean Createsubtask(Subtask st);
+        public Subtask ReadSubtask(int task_id);
+
+        public Boolean Updatesubtask(Subtask st);
+        public Boolean Deletesubtask(Subtask st);
+
+
     }
 }
---
+++ ViewModel/PostViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TMS.Models;
+
+namespace TMS.ViewModel
+{
+    public class PostViewModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string desc { get; set; }
+
+        public DateTime sdate { get; set; }
+        public DateTime fdate { get; set; }
+        public string state { get; set; }
+
+        List<Subtask> s = new List<Subtask>();
+            
+    }
+}
